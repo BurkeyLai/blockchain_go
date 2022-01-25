d6e5eb60390bd96ca6dbea1b53faf9db9e7e2f55
BurkeyLai <imugly1029@gmail.com>
2022-01-26 00:27:15 +0800

    test1

--- base58_test.go
+++ base58_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/hex"
-	"log"
 	"strings"
 	"testing"
 
@@ -13,7 +12,7 @@ func TestBase58(t *testing.T) {
 	rawHash := "00010966776006953D5567439E5E39F86A0D273BEED61967F6"
 	hash, err := hex.DecodeString(rawHash)
 	if err != nil {
-		log.Fatal(err)
+		//	log.Fatal(err)
 	}
 
 	encoded := Base58Encode(hash)
